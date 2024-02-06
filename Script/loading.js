00215e55b74e836f55b8c916a72ff83de9f0d5ff
NardHodo <133963030+NardHodo@users.noreply.github.com>
2024-02-06 16:23:01 +0800

    Add files via upload

--- Script/loading.js
+++ Script/loading.js
@@ -38,3 +38,19 @@ function removeAnimation(){
 
 setInterval(removeAnimation, 2500);
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.querySelectorAll('.nav-link');
+  
+    links.forEach(function(link) {
+      link.addEventListener('click', function() {
+       
+        links.forEach(function(l) {
+          l.classList.remove('active');
+        });
+  
+        this.classList.add('active');
+      });
+    });
+  });
+  
